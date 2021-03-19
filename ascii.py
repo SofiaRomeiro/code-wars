1cf8f9ccc5533d7830076c4f73ec15ec23d09156
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2021-03-19 20:50:52 +0000

    trainee

---
+++ ascii.py
@@ -0,0 +1,26 @@
+def comparador(s):
+    val = 0
+    st_l = []
+    
+    for c in s:
+        if ord(c) != ord(' '):
+            st_l += [c]  
+ 
+    if len(st_l) % 2 == 0:
+        while st_l != []:
+            if st_l[0] != st_l[-1]:
+                return 0
+            st_l = st_l[1:-1]
+
+    else:        
+        while len(st_l) > 1:
+            if st_l[0] != st_l[-1]:
+                return 0
+            st_l = st_l[1:-1]
+    return 1   
+
+s = eval(input("Escreva uma frase?\n"))
+print(comparador(s))
+
+
+
